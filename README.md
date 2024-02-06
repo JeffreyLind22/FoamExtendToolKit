6bdf2a5973f9d3273e74d1bc996e4d49d5a658f8
jlind3 <jeffrey@jeffreylind.dev>
2024-02-05 22:11:16 -0500

    Added foamExtendSourceUbuntu.sh

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Some words
+# FoamExtendToolKit
+
+## Scripts
+
+### foamExtendSourceUbuntu.sh
+`foamExtendSourceUbuntu.sh` installs all required dependencies and installs `foam-extend-5.0` from source for Ubuntu 22.04. It also echos the `foam-extend-5.0` source alias `fe50` into `$HOME/.bashrc`, which will source `foam-extend-5.0/etc/bashrc`. Sudo access is required. Based off of [these instructions](https://sourceforge.net/p/foam-extend/foam-extend-5.0/ci/master/tree/doc/buildInstructions/Debian/Ubuntu/Ubuntu_22.04).
+
+To run:
+
+```bash
+bash foamExtendSourceUbuntu.sh
+```
---
+++ foamExtendSourceUbuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd ~
+
+mkdir foam
+
+cd foam
+
+git clone git://git.code.sf.net/p/foam-extend/foam-extend-5.0 foam-extend-5.0
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install git binutils-dev cmake flex zlib1g-dev libncurses5-dev curl bison libxt-dev rpm mercurial graphviz openmpi-bin libopenmpi-dev build-essential linux-generic libmpich-dev libopenmpi-dev metis libmetis-dev libparmetis-dev scotch libscotch-dev python3 python3-dev python-is-python3 python3-setuptools
+
+cd foam-extend-5.0
+
+sed -i -e 's=rpmbuild --define=rpmbuild --define "_build_id_links none" --define=' ThirdParty/tools/makeThirdPartyFunctionsForRPM
+
+. etc/bashrc
+
+./Allwmake.firstInstall -j -l -s
+
+mkdir -p $FOAM_RUN
+
+echo "alias fe50='source \$HOME/foam/foam-extend-5.0/etc/bashrc'" >> $HOME/.bashrc
